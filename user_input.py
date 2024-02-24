2337b14784013ea270400cfdf7732a29d9569b9a
isaacsabbath <isaacsabbath002@gmail.com>
2024-02-24 23:07:47 +0300

    py assignment1

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# using input functionto ask user for age, namee and location
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+print(f'Hello, {name}, you are {age} old and you live in {location}')
